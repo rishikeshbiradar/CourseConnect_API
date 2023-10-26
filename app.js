b3e8393631326ce4a93407c849db086b0fa27228
Rushikesh Shivaji Biradar <115314561+rishikeshbiradar@users.noreply.github.com>
2023-10-27 02:42:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 5000
+require("dotenv").config();
+const ConnectToDatabase = require("./db/connect")
+const Course_Router=require("./routes/course")
+
+app.get("/", (req, res) =>
+{
+    res.send("Hi I am Rushikesh Biradar")
+})
+
+app.use("/api/courses", Course_Router);
+
+const start =async () =>
+{
+    try {
+        ConnectToDatabase(process.env.MONGODB_URL);
+        app.listen(PORT, () =>
+        {
+            console.log(`Server is Started on PORT ${PORT}`)
+        })
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
